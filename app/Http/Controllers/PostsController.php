6dc30752cb15d82c5b734a3b927a99168004c749
Anastasia Yugay <anastasia.3141592@gmail.com>
2022-07-02 21:18:27 +0300

    Upload to cloudinary

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use App\Models\User;
+use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
+use Cloudinary\Tag\ImageTag;
+use Cloudinary\Transformation\Resize;
 use Illuminate\Http\Request;
 use Intervention\Image\Facades\Image;
 
@@ -40,9 +43,16 @@ class PostsController extends Controller
         $image = Image::make(public_path("$image_path"))->fit(1200, 1200);
         $image->save();
 
+
+        $uploadedFileUrl = Cloudinary::upload(request('image')->getRealPath(), 
+            ["folder" => "uploads", 
+            "width" => 1200, "height" => 1200, 'crop' => 'fill']
+            )->getSecurePath();
+
         $post = auth()->user()->posts()->create([
             'caption' => $data['caption'],
-            'image' => $image_path,
+            //'image' => $image_path,
+            'image' => $uploadedFileUrl,
         ]);
 
         return redirect('/profile/'.auth()->user()->id);
